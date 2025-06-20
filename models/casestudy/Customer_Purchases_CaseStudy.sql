5c3d0466da7b67420e7743c18092ab8a68ad3991
Priyal Agrawal <Priyal.Agrawal@in.ey.com>
2025-06-20 05:00:10 +0000

    Updated sql query

--- models/casestudy/Customer_Purchases_CaseStudy.sql
+++ models/casestudy/Customer_Purchases_CaseStudy.sql
@@ -1,6 +1,5 @@
-INSERT INTO mydb.postgres_casestudy_myschema.combined_table (CUSTOMER_ID, NAME, CATEGORY, PRODUCT, PRICE)
 SELECT CUSTOMER_ID, NAME, CATEGORY, PRODUCT, PRICE_INR_
 FROM mydb.onedrive_casestudy.customer_data_sheet_1
 UNION ALL
 SELECT CUSTOMER_ID, NAME, CATEGORY, PRODUCT, PRICE
-FROM mydb.postgres_casestudy_myschema.customer_purchases;
+FROM mydb.postgres_casestudy_myschema.customer_purchases
